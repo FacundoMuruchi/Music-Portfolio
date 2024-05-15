a7871b98cc234de31986065b3ebbdd9f9775486d
FacundoMuruchi <facundomuruchi01@gmail.com>
2024-05-14 22:25:49 -0300

    projects and resume

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import resume, projects, contact
+from .views import resume, projects, contact, generate_pdf
 
 urlpatterns = [
     path('resume/', resume, name='resume'),
     path('projects/', projects, name='projects'),
     path('contact/', contact, name='contact'),
+    path('download/', generate_pdf, name='download'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
 # Create your views here.
 def resume(request):
@@ -18,3 +21,26 @@ def contact(request):
         request=request,
         template_name='mysite/contact.html'
     )
+
+def generate_pdf(request):
+    # Creamos un objeto HttpResponse con el tipo de contenido pdf
+    response = HttpResponse(content_type='application/pdf')
+    # Especificamos el nombre del archivo como adjunto
+    response['Content-Disposition'] = 'attachment; filename="Facundo_Muruchi_Resume.pdf"'
+
+    # Creamos el documento PDF
+    p = canvas.Canvas(response)
+
+    # Obtener el tamaño de la página
+    width, height = letter
+
+    # Calcular la coordenada y de la esquina superior izquierda
+    y_top_left = height
+
+    # Dibujar el texto en la esquina superior izquierda
+    p.drawString(40, y_top_left, "Este es mi currículum")
+    # Cerramos el objeto Canvas
+    p.showPage()
+    p.save()
+
+    return response
