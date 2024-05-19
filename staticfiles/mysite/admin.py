0fa4b63cefa5464719ceb6f39d13841ea31a8291
FacundoMuruchi <facundomuruchi01@gmail.com>
2024-05-19 20:39:52 -0300

    imgs

--- staticfiles/mysite/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Com, Web, Curriculum, Data
-
-# Register your models here.
-
-admin.site.register(Com)
-admin.site.register(Web)
-admin.site.register(Curriculum)
-admin.site.register(Data)
--- staticfiles/portfolio/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def inicio(request):
-    return render(
-        request=request,
-        template_name='home.html'
-    )
