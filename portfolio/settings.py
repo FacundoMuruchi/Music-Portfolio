ff293432965d0ea5357b0feef25075858405198e
FacundoMuruchi <facundomuruchi01@gmail.com>
2024-10-02 20:36:04 -0300

    witches road

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wkc^cd%-5_iq_^el&h4#2dw7ww=pv(60%i-)njy@4db@tf=4me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['fmuruchi.pythonanywhere.com']
+ALLOWED_HOSTS = ['fmuruchi.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
